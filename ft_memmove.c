16dd1188af8ad7a376ccd4ee27c76edf80217d82
Samy AICHAOUI <saichaou@e2r3p7.clusters.42paris.fr>
2022-11-09 16:38:21 +0100

    final maison 09/11

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: saichaou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 16:15:17 by saichaou          #+#    #+#             */
-/*   Updated: 2022/11/09 16:22:38 by saichaou         ###   ########.fr       */
+/*   Updated: 2022/11/09 16:37:50 by saichaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	
+	int	i;
+	unsigned char	*temp;
+
+	i = 0;
+	while (i < n)
+	{
+		temp 
+	}
 }
