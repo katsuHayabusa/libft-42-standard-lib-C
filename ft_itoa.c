0dd9a826b9a8f2892dc6c779925bd486bbba1aa5
Samy AICHAOUI <saichaou@e2r9p18.clusters.42paris.fr>
2022-11-14 15:51:26 +0100

    E

---
+++ ft_itoa.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saichaou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 15:00:26 by saichaou          #+#    #+#             */
+/*   Updated: 2022/11/14 15:51:10 by saichaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static  int	num_size(int n)
+{
+	int	i;
+
+	i = 0;
+	while (n > 10)
+	{
+		i++;
+		n /= 10;
+	}
+	return (i + 1);
+}
+
+char	*ft_itoa(int n)
+{
+	int     i;
+	char	*str;
+
+	if (n == 0)
+		return ("0");
+	str = malloc(num_size(n) * sizeof(int));
+	i = num_size(n) - 1;
+	while (n > 10)
+	{
+		str[i] = (n % 10) + '0';
+		n /= 10;
+		i--;
+	}
+	str[i] = n + '0';
+	return (str);
+}
---
+++ ft_strjoin.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saichaou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 13:25:06 by saichaou          #+#    #+#             */
+/*   Updated: 2022/11/14 13:39:34 by saichaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int	i;
+	int	j;
+	char	*res;
+
+	if (!s1 && !s2)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
+	res = malloc((i + j * sizeof(char)));
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	while(s2[j])
+	{
+		res[i + j] = s2[j];
+		j++;
+	}
+	return (res);
+}
