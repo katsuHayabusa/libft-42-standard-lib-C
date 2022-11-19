d400ffbee8bb5617fabd2e02beb2cedb86374d06
Samy AICHAOUI <saichaou@e1r2p19.clusters.42paris.fr>
2022-11-19 16:16:56 +0100

    salut

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: saichaou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 14:53:30 by saichaou          #+#    #+#             */
-/*   Updated: 2022/11/19 16:02:17 by saichaou         ###   ########.fr       */
+/*   Updated: 2022/11/19 16:16:17 by saichaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,14 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	i;
 	size_t	j;
 
-	if (*src == '\0')
-		return (ft_strlen(dst));
+	j = ft_strlen(dst);
+	if (!ft_strlen((char *)src))
+		return (j);
 	i = 0;
-	while (dst[i])
-		i++;
-	j = i - 1;
-	i = 0;
-	while (i < size - j - 1 && src[i])
+	while (j < size - 1 && src[i])
 	{
-	dst[i + j] = src[i];
-	i++;
+		dst[j + i] = src[i];
+		i++;
 	}
-	dst[i] = '\0';
-	return (i);
+	return (i + j);
 }
