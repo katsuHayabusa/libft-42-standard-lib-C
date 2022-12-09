5623bac84aad2fda0d737e46ae946fe01df52493
Samy AICHAOUI <saichaou@e2r5p11.clusters.42paris.fr>
2022-12-09 14:51:41 +0100

    dd

--- ft_split.c
+++ ft_split.c
@@ -6,21 +6,46 @@
 /*   By: saichaou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 13:54:40 by saichaou          #+#    #+#             */
-/*   Updated: 2022/12/08 11:30:06 by saichaou         ###   ########.fr       */
+/*   Updated: 2022/12/09 14:51:10 by saichaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+size_t	wcount(char const *s, char c)
+{
+	size_t	i;
+
+	i = 1;
+	while (s)
+	{
+		if (*s == c)
+			i++;
+		s++;
+	}
+	return (i);
+}
+
+size_t	charcount(char const *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (s && *s == c)
+		i++;
+	return (i);
+}
 
 
 char	**split(char const *s, char c)
 {
-	int	i;
-	char **gg;
-	gg = malloc(5 * sizeof(char *));
-	i = 0;
-	while (s[i++])
-		c = s[i];
-	return (gg);
+	char	**split;
+	int		i;
+	int		j;
+
+	split = malloc(wcount(s, c) * sizeof(char *));
+	if (!split)
+		return (NULL);
+	split[0] = malloc(charcount(char const *s, c));
+	while (s)
 }
