210e227c0fce21eb040a7f557593f2b57d3ccb53
Samy AICHAOUI <saichaou@e1r7p15.clusters.42paris.fr>
2022-12-22 14:53:03 +0100

    ramenez la coupe a la maison

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: saichaou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 02:34:02 by saichaou          #+#    #+#             */
-/*   Updated: 2022/12/14 16:29:36 by saichaou         ###   ########.fr       */
+/*   Updated: 2022/12/22 14:43:51 by saichaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*list;
 
-	if (*lst == NULL)
+	if (*lst == NULL || lst == NULL)
 		*lst = new;
 	else
 	{
--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: saichaou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 12:22:30 by saichaou          #+#    #+#             */
-/*   Updated: 2022/12/14 19:17:00 by saichaou         ###   ########.fr       */
+/*   Updated: 2022/12/22 14:49:37 by saichaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*tmp;
 
-	if (!lst || !del)
+	if (!lst || !del || !*lst)
 		return ;
 	while (*lst)
 	{
