d5ce1994bb9a854921a7d22331eb7d85a11ef7fb
Samy AICHAOUI <saichaou@e1r9p16.clusters.42paris.fr>
2022-12-25 16:09:26 +0100

    salut mon pote

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,23 +6,23 @@
 /*   By: saichaou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 12:47:45 by saichaou          #+#    #+#             */
-/*   Updated: 2022/12/22 17:08:03 by saichaou         ###   ########.fr       */
+/*   Updated: 2022/12/25 16:09:05 by saichaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t n, size_t size)
+void    *ft_calloc(size_t nmemb, size_t size)
 {
-	size_t	total;
-	void	*res;
+    void	*ptr;
+	size_t	j;
 
-	total = n * size;
-	if (total && n != total / size)
+	j = size * nmemb;
+	if (j && nmemb != j / size)
 		return (NULL);
-	res = malloc(total);
-	if (!res)
-		return (NULL);
-	ft_bzero(res, total);
-	return (res);
+	ptr = malloc(j);
+	if (!size)
+		return ((void *) 0);
+    ft_bzero(ptr, j);
+    return (ptr);
 }
